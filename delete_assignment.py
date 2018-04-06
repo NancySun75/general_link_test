63e92a9b7ea766b28f713da4ad17d422345f906d
nancy <honeyhoneystar@126.com>
2018-04-06 16:36:58 +0800

    Begin to delete assginment

--- delete_assignment.py
+++ delete_assignment.py
@@ -2,28 +2,11 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.action_chains import ActionChains
 
-def delete_asmt(driver):
-	found = False
-	while found == False:
-		rows = driver.find_elements_by_css_selector(".md-table-row")
-		found = find_in_current_page(rows)
-		if found == False:
-			next_page = driver.find_element_by_css_selector("#data-table-pagination-increment-btn")
-			next_page.click()
-
-
-def find_in_current_page(rows):
+def delete_asmt(driver, asmt_name):
+	rows = driver.find_elements_by_css_selector(".md-table-row")
 	for row in rows:
 		name = row.find_element_by_css_selector("span").text
 		print (name, "================")
-		if "04030646_Ren_QA" == name:
+		if asmt_name == name:
 			three_point = row.find_element_by_css_selector('[aria-label="Additional Options"]')
 			three_point.click()
-			delete_li = driver.find_elements_by_css_selector("ul.md-list li")[3]
-			disabled = delete_li.find_element_by_css_selector('div').get_attribute("disabled")
-			if disabled == False:
-				delete_li.click()
-			else:
-				print "This assignment is disabled to delete."
-			return True
-	return False
