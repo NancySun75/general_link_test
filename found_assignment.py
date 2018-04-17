588de20a66ad1b818d58bb7e1b3d5ecfe55472aa
nancy <honeyhoneystar@126.com>
2018-04-18 06:42:34 +0800

    student_open_asmt_page

--- found_assignment.py
+++ found_assignment.py
@@ -7,16 +7,17 @@ def found_asmt_by_page(driver, asmt_name):
 	while found == False:
 		next_page = driver.find_element_by_css_selector("#data-table-pagination-increment-btn")
 		next_page.click()
-		found = found_delete_asmt(driver, asmt_name)
+		found = found_asmt(driver, asmt_name)
 
 def found_asmt(driver, asmt_name):
-	rows = driver.find_elements_by_css_selector(".md-table-row")
+	rows = driver.find_elements_by_css_selector('[aria-label="Open assignment"]')
 	for row in rows:
-		name = row.find_element_by_css_selector("a").text
+		name = row.text
 
 		if asmt_name == name:
 			open_asmt(driver, row)
-	return True
+			return True
+	return False
 
 def open_asmt(driver, row):
 	row.click()
